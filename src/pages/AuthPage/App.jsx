8b17a350f9c2373197c70a96bfdc225715b574b6
Yishaq Woldesenebet <yishaq6@gmail.com>
2022-11-10 10:47:03 -0500

    new folders commit

--- src/pages/AuthPage/App.jsx
+++ src/pages/AuthPage/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import SignUpForm from "../components/SignUpForm/SignUpForm";
-import LoginForm from "../components/LoginForm/LoginForm";
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import LoginForm from "../../components/LoginForm/LoginForm";
 
 const AuthPage = ({ setUser }) => {
   const [showSignUp, setShowSignUp] = useState(true);
